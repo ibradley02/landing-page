bd6e39fa8ab597c836bd180d217db4bacaac2872
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-20 15:21:00 -0600

    commiting changes

--- scripts/jsconfig.js
+++ scripts/jsconfig.js
@@ -1,15 +1,15 @@
-$('.flip').hover(function(){
+$('.flip').hover(function () {
     $(this).find('.card').toggleClass('flipped');
 });
 
-$('#toggle').on('hover' ,function(){
-    $('.card').toggleClass('flipped');
-});
+// $('#toggle').on('hover' ,function(){
+//     $('.card').toggleClass('flipped');
+// });
 
-$('#front').on('hover' ,function(){
-    $('.card').addClass('flipped');
-});
+// $('#front').on('hover' ,function(){
+//     $('.card').addClass('flipped');
+// });
 
-$('#back').on('hover' ,function(){
-    $('.card').removeClass('flipped');
-});
+// $('#back').on('hover' ,function(){
+//     $('.card').removeClass('flipped');
+// });
