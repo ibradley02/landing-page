a1ef107b527f92801a8f98b2bc52ada2e98d27c0
Ian Bradley <ianbradleydebate@gmail.com>
2017-12-16 18:43:38 -0700

    finished vue front end setup, created navbar and home vue

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <navbar></navbar>
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row text-center">
+        <nav class="navbar navbar-default navbar-fixed-top myNavbar">
+            <h1>Ian Bradley</h1>
+        </nav>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'navbar',
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+        components: {
+        },
+        computed: {
+        }
+    }
+</script>
+
+
+
+<style>
+    .myNavbar {
+        background-color: rgba(0, 1, 3, 0);
+        display: flex;
+        margin-bottom: 0px;
+    }
+    .navbar {
+        border: 0px;
+    }
+    .navbar-btn {
+        background: black;
+        border: 0px;
+    }
+</style>
