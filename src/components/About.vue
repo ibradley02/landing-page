3576b44e9201e06f23bf7f5263bf293e3ee653ff
Ian Bradley <ianbradleydebate@gmail.com>
2018-01-04 15:44:43 -0700

    change to about and resume

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,34 @@
 <template>
     <div>
         <navbar></navbar>
-        <div class="main">
-            <header>
-                <h1>About</h1>
+        <div class="main row">
+            <header class="col-sm-12">
+                <h1>Ian Bradley</h1>
             </header>
+            <div class="row">
+                <div class="col-sm-4 panel panel-default">
+                    <div class="panel-heading">
+                        <h4>About Me</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ut aliquid nostrum laborum quisquam.
+                            Asperiores exercitationem quaerat sequi hic ad reprehenderit sint minima, animi pariatur perspiciatis
+                            commodi, eveniet atque ex dolores aspernatur amet esse blanditiis ipsum? Autem enim tenetur illum
+                            magni corporis deserunt, veritatis explicabo. Et exercitationem a sequi quae!</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 panel panel-default col2">
+                    <div class="panel-heading">
+                        <h4>Objective</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic facilis iste libero quam aspernatur
+                            quae pariatur omnis velit ad dolore delectus aliquid aut consectetur tempore blanditiis veritatis,
+                            non molestiae quod officia nemo voluptate modi similique? Distinctio perspiciatis laborum, deleniti
+                            ipsum molestias molestiae, nesciunt consequuntur alias ex itaque ullam debitis!</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,4 +50,12 @@
 </script>
 
 <style>
+    .panel {
+        float: none;
+        margin: 0 auto;
+        display: inline-block;
+    }
+    .col2 {
+        margin-left: 2vw;
+    }
 </style>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
         <navbar></navbar>
-        <div class="main">
-            <header>
-                <h1>Resume</h1>
+        <div class="main row">
+            <header class="col-sm-12">
+                <h1>Ian Bradley</h1>
+                <div class="email">
+                    <h5>Email: ianbradleydebate@gmail.com -</h5>
+                </div>
+                <div class="phone">
+                    <h5>Phone: 208-871-6738</h5>
+                </div>
             </header>
+            <div class="col-sm-6 panel panel-default">
+                <div class="panel-heading">Education</div>
+                <div class="panel-body">
+                    <ul>
+                        <li>
+                            Rocky Mountain High School, Graduated: May, 2015
+                        </li>
+                        <li>
+                            Collegiate Enrollment: Northwest Nazarene University, Boise State University
+                        </li>
+                        <li>
+                            Boise CodeWorks Expected Graduate: Jan. 2018
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 panel panel-default">
+                <div class="panel-heading">Work History</div>
+                <div class="panel-body"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,4 +52,11 @@
 </script>
 
 <style>
+    .email {
+        display: inline-flex;
+    }
+
+    .phone {
+        display: inline-flex;
+    }
 </style>
