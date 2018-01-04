4e203e801d04d1d9fef28628bf18c741bcff3773
Ian Bradley <ianbradleydebate@gmail.com>
2018-01-04 16:03:54 -0700

    styling changes to cards

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,26 +6,30 @@
                 <h1>Ian Bradley</h1>
             </header>
             <div>
-                <div class="col-sm-4 panel panel-default">
-                    <div class="panel-heading">
-                        <h4>About Me</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ut aliquid nostrum laborum quisquam.
-                            Asperiores exercitationem quaerat sequi hic ad reprehenderit sint minima, animi pariatur perspiciatis
-                            commodi, eveniet atque ex dolores aspernatur amet esse blanditiis ipsum? Autem enim tenetur illum
-                            magni corporis deserunt, veritatis explicabo. Et exercitationem a sequi quae!</p>
+                <div class="col-sm-4 panel-style">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>About Me</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ut aliquid nostrum laborum quisquam.
+                                Asperiores exercitationem quaerat sequi hic ad reprehenderit sint minima, animi pariatur perspiciatis
+                                commodi, eveniet atque ex dolores aspernatur amet esse blanditiis ipsum? Autem enim tenetur illum
+                                magni corporis deserunt, veritatis explicabo. Et exercitationem a sequi quae!</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-4 panel panel-default col2">
-                    <div class="panel-heading">
-                        <h4>Objective</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic facilis iste libero quam aspernatur
-                            quae pariatur omnis velit ad dolore delectus aliquid aut consectetur tempore blanditiis veritatis,
-                            non molestiae quod officia nemo voluptate modi similique? Distinctio perspiciatis laborum, deleniti
-                            ipsum molestias molestiae, nesciunt consequuntur alias ex itaque ullam debitis!</p>
+                <div class="col-sm-4 col2 panel-style">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Objective</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic facilis iste libero quam aspernatur
+                                quae pariatur omnis velit ad dolore delectus aliquid aut consectetur tempore blanditiis veritatis,
+                                non molestiae quod officia nemo voluptate modi similique? Distinctio perspiciatis laborum, deleniti
+                                ipsum molestias molestiae, nesciunt consequuntur alias ex itaque ullam debitis!</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,7 +54,7 @@
 </script>
 
 <style>
-    .panel {
+    .panel-style {
         float: none;
         margin: 0 auto;
         display: inline-block;
