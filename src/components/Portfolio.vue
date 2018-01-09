82dbed48967c27fd1c36cca8311a409d39f7a7e5
ianbradleydebate@gmail.com <you@example.com>
2018-01-08 21:40:58 -0700

    button @click changed to @mouseover changes to panel styling

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,24 +1,19 @@
 <template>
-    <div class="row">
+    <div>
         <navbar></navbar>
         <div class="main row">
             <header>
                 <h1>Portfolio</h1>
             </header>
-            <div class="col-sm-3" v-if="info.show">
-                <a @click="toggleShowInfo" class="btn btn-default panel-style">
+            <div class="col-sm-3">
+                <div @mouseover="info.show = true" @mouseleave="info.show = false" class="btn btn-default panel-style content">
                     <img src="https://i.imgur.com/JQs05hS.png" alt="A Kanban Website">
-                </a>
-            </div>
-            <div class="col-sm-3" v-else>
-                <a @click="toggleShowInfo" class="btn btn-default panel-style">
-                    <div class="content row">
-                        <img src="https://i.imgur.com/JQs05hS.png" alt="A Kanban Website">
+                    <div v-show="info.show" class="row">
                         <p class="col-sm-12">A Kanban application made in vue with node.js</p>
                         <a class="col-sm-6" href="https://github.com/ibradley02/vue-kanban" target="_blank">View Code</a>
                         <a class="col-sm-6" href="http://kanban-track-it.herokuapp.com/#/" target="_blank">Visit Website</a>
                     </div>
-                </a>
+                </div>
             </div>
             <div class="col-sm-3">
                 <a href="http://kanban-track-it.herokuapp.com/#/" class="btn btn-default panel-style">
@@ -40,60 +35,58 @@
 </template>
 
 <script>
-    import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 
-    export default {
-        name: "Portfolio",
-        data() {
-            return {
-                info: {
-                    show: true
-                }
-            };
-        },
-        components: {
-            Navbar
-        },
-        methods: {
-            toggleShowInfo() {
-                this.info.show = !this.info.show;
-            }
-        }
+export default {
+  name: "Portfolio",
+  data() {
+    return {
+      info: {
+        show: true
+      }
     };
+  },
+  components: {
+    Navbar
+  },
+  methods: {
+    toggleShowInfo() {
+      this.info.show = !this.info.show;
+    }
+  }
+};
 </script>
 
 <style scoped>
-    img {
-        height: 100%;
-        width: 100%;
-    }
+img {
+  height: 100%;
+  width: 100%;
+}
 
-    .panel-style {
-        padding: 1rem 1rem 1rem 1rem;
-        margin: 0 auto;
-    }
+.panel-style {
+  padding: 1rem 1rem 1rem 1rem;
+  margin: 0 auto;
+}
 
-    .col-sm-3 {
-        margin: 0 auto 2vh;
-        padding: 1rem 1rem 1rem 1rem;
-    }
+.col-sm-3 {
+  margin: 0 auto;
+  padding: 1rem 1rem 1rem 1rem;
+}
 
-    .content {
-        position: relative;
-    }
+.content {
+  position: relative;
+}
 
-    p {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        margin: auto auto;
-        bottom: 0;
-        background: rgba(211, 211, 211, 0.600);
-        padding: 10px 10px;
-        color: black;
-    }
-
-    p {
-        display: block;
-    }
+.boxed-content {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  background: rgba(211, 211, 211, 0.3);
+  color: black;
+  display: block;
+  padding: 1rem 1rem 1rem 1rem;
+  margin: 0 auto 0 5px;
+  background-clip: content-box;
+}
 </style>
